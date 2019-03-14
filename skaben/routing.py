9782c3219952f0a4da6d2c4553740658c1bf36aa
Dmitry Bis <foxtrot@rinet.ru>
2019-03-14 16:26:19 +0300

    minor fixes

--- skaben/routing.py
+++ skaben/routing.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from channels.routing import ProtocolTypeRouter, ChannelNameRouter
 from sk_mqtts.consumers import MQTTConsumer
+from sk_iface.consumers import EventConsumer
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
     "channel": ChannelNameRouter({
-        "mqtt_send": MQTTConsumer,
-        "app_control": MQTTConsumer,
+        "mqtts": MQTTConsumer,
+        "events": EventConsumer,
     }),
 })
