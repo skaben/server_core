fd80123f4d03fbb0da43dcd1a6e1ff4a70a04a62
Dmitry Bis <foxtrot@rinet.ru>
2019-03-20 20:19:02 +0300

    minor changes in styles

--- sk_iface/consumers.py
+++ sk_iface/consumers.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from .event_contexts import DeviceEventContext
 from asgiref.sync import async_to_sync
 from channels.consumer import SyncConsumer
@@ -27,6 +28,8 @@ class EventConsumer(SyncConsumer):
         :param msg: message from Redis-backed channel layer
         :return:
         """
+        self._send_ws_log(msg)
+        # next - send command
         with DeviceEventContext(msg) as dev:
 
             # initialize event context with received message
@@ -65,7 +68,7 @@ class EventConsumer(SyncConsumer):
             else:
                 print(f'command {dev} not implemented')
 
-    def send_log(self, data=None):
+    def _send_ws_log(self, data=None):
         """
             cross-connection with webeventconsumer for sending messages to weblog
         :param data:
@@ -74,11 +77,11 @@ class EventConsumer(SyncConsumer):
         if not data:
             print('no data')
             return
-        print(f'EVENT CONSUMER SENDLOG as: {data}')
         msg = {
-            'type': 'send.log',
-            'data': json.dumps(data),
+            "type": "send.log",
+            "data": data,
         }
+        print('sending', msg['data'])
         async_to_sync(self.channel_layer.group_send)('ws_send', msg)
 
 
@@ -101,8 +104,8 @@ class WebEventConsumer(AsyncConsumer):
 
     async def websocket_receive(self, event):
         await self.channel_layer.send('events', {
-            'type': 'send.log',
-            'data': event,
+            "type": 'websocket.event',
+            "data": event,
         })
 #        await self.send({
 #            "type": "websocket.send",
@@ -110,8 +113,9 @@ class WebEventConsumer(AsyncConsumer):
 #        })
 
     async def send_log(self, data):
-        print(f'received: {data}')
+        timestamp = datetime.now().strftime('%X')
+        data.update({'time': timestamp})
         await self.send({
             "type": "websocket.send",
-            "text": 'RECEIVED, ONLY DUMP MISSING',
+            "text": str(data)
         })
