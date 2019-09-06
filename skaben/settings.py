2dc1ff3d18c7ab4cc6d03b1bd88177a108b4545e
Dmitry Bis <foxtrot@rinet.ru>
2019-09-06 14:14:28 +0300

    mqtt module settings now in global app settings

--- skaben/settings.py
+++ skaben/settings.py
@@ -264,5 +264,22 @@ APPCFG = {
     'timeout': 1,  # between pings
     'alive': 60,  #  set device offline after
     'text_storage': 'misc/texts', # texts directory
-    'tz': 'Europe/Moscow' # timezone
+    'tz': 'Europe/Moscow', # timezone
+    'debug': True,
+    'mqtt': {
+        'host': '127.0.0.1',
+        'port': 1883
+    },
+    'timeouts': {
+        'device_keepalive': 15,
+        'broker_keepalive': 10,
+        'ping': 1
+    },
+    'device_types': [
+        'term',
+        'lock',
+        'rgb',
+        'pwr',
+        'aux'
+    ]
 }
