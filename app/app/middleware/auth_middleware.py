1106d3a68db2bbb2940b52aa1cc8a30baa5c280d
zerthmonk <zerthmonk@pm.me>
2020-02-01 13:01:44 +0300

    permission denied middleware

---
+++ app/app/middleware/auth_middleware.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+
+
+class AuthRequiredMiddleware(object):
+
+    allowed = (
+        '/auth/token',
+        '/favicon'
+    )
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if not request.path_info.startswith(self.allowed) and not request.user.is_authenticated:
+            return HttpResponse('permission denied for unauthorized user', status=401)
+        return response
