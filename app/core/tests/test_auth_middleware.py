73c2e0f04b07b44bcd8a8cfbed06285d8437dcc8
zerthmonk <zerthmonk@pm.me>
2020-02-01 13:01:55 +0300

    some dumb token tests

---
+++ app/core/tests/test_auth_middleware.py
@@ -0,0 +1,42 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from tools_testing.helpers import create_user
+
+ROOT_URL = '/'
+AUTH_URL = reverse('token')
+
+
+class TestAuthMiddlewareNotAuthenticated(APITestCase):
+    """Test the publicly available locks API"""
+
+    def test_login_required(self):
+        """Test that login required for retrieving tags"""
+        res = self.client.get(ROOT_URL)
+
+        assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_token_url_accessible(self):
+        """ Test that token url is accessible without authorization """
+        res = self.client.get(AUTH_URL)
+
+        assert res.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+    def test_token_authorization_success(self):
+        """ Test that client receives authorization token success """
+        payload = {'username': 'test',
+                   'password': 'testing'}
+        create_user(**payload)
+
+        res = self.client.post(AUTH_URL, payload)
+
+        assert res.status_code == status.HTTP_200_OK
+
+    def test_token_authorization_fail(self):
+        """ Test that client receives authorization token fail """
+        payload = {'username': 'test',
+                   'password': 'testing'}
+
+        res = self.client.post(AUTH_URL, payload)
+
+        assert res.status_code == status.HTTP_400_BAD_REQUEST
--- app/tools_testing/helpers.py
+++ app/tools_testing/helpers.py
@@ -1,6 +1,11 @@
 import string
 import random
+from django.contrib.auth import get_user_model
 
 
 def gen_random_str(size=8, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choices(chars, k=size))
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
