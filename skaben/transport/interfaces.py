cf332f7d9fd1a4d9416a1f6582af94fa233305d0
Dmitry Bis <zerthmonk@users.noreply.github.com>
2020-07-22 09:40:35 +0000

    using pool

--- skaben/transport/interfaces.py
+++ skaben/transport/interfaces.py
@@ -1,25 +1,22 @@
 import json
 import time
 
-from kombu import Connection
-
 from django.conf import settings
-from transport.rabbitmq import exchanges
+from transport.rabbitmq import exchanges, pool, connection
 
 
 def send_plain(topic, data):
-    with Connection(settings.AMQP_URL) as conn:
-        with conn.channel() as channel:
-            prod = conn.Producer(channel)
-            try:
-                prod.publish(data,
-                             exchange=exchanges.get('mqtt'),
-                             routing_key=f"{topic}")
-                return f'success: {topic} with {data}'
-            except Exception as e:
-                return dict(payload=f"FAILED: {topic} with {data} >> {e}",
-                            exchange=exchanges.get('log'),
-                            routing_key="error")
+    with pool.acquire() as channel:
+        prod = connection.Producer(channel)
+        try:
+            prod.publish(data,
+                         exchange=exchanges.get('mqtt'),
+                         routing_key=f"{topic}")
+            return f'success: {topic} with {data}'
+        except Exception as e:
+            return dict(payload=f"FAILED: {topic} with {data} >> {e}",
+                        exchange=exchanges.get('log'),
+                        routing_key="error")
 
 
 def send_message(topic, uid, command, payload={}):
