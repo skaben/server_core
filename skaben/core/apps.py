fec5a8adceb45c1ccf5ad1c74c2d6dbe891637a4
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2020-12-26 12:34:46 +0200

    run workers and pinger on server start

--- skaben/core/apps.py
+++ skaben/core/apps.py
@@ -1,5 +1,13 @@
+import sys
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+    def ready(self):
+        if 'runserver' in sys.argv:
+            from core.tasks.main import run_workers, run_pinger
+            print('running workers')
+            run_workers()
+            run_pinger()
