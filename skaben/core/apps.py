2c6be8b8e2fb12ceea19828477d4684178cbbd70
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2021-04-18 23:19:24 +0300

    alert management, current switch now in model

--- skaben/core/apps.py
+++ skaben/core/apps.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from django.apps import AppConfig
 
@@ -9,6 +10,8 @@ class CoreConfig(AppConfig):
     def ready(self):
         if 'runserver' in sys.argv:
             from transport.tasks.main import run_pinger, run_workers
-            print('running workers')
+            print('starting workers')
             run_workers()
+            print('starting pinger')
+            print('********** SKABEN SYSTEMS UP. SERVING THE OMNISSIAH **********')
             run_pinger()
