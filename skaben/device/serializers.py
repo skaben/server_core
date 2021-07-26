d12ef511faa2ec628d86faf05deb62ad3c5ee117
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2021-07-27 01:32:23 +0300

    Добавлены сериализаторы для терминала

--- skaben/device/serializers.py
+++ skaben/device/serializers.py
@@ -1,3 +1,6 @@
+import json
+from hashlib import md5
+
 from rest_framework import serializers
 from transport.interfaces import send_message_over_mqtt
 
@@ -6,6 +9,8 @@ from shape.serializers import WorkModeSerializer
 
 
 class DeviceSerializer(serializers.ModelSerializer):
+
+    topic = ''
 
     alert = serializers.ReadOnlyField()
 
@@ -17,6 +22,11 @@ class DeviceSerializer(serializers.ModelSerializer):
             return super().save()
         self.send_config()
         super().save()
+
+    @staticmethod
+    def get_hash_from(data: dict) -> str:
+        dump = json.dumps(data).encode('utf-8')
+        return md5(dump).hexdigest()
 
     def send_config(self):
         """Отправляем конфиг клиенту.
@@ -35,6 +45,16 @@ class LockSerializer(DeviceSerializer):
 
     acl = serializers.ReadOnlyField()
     online = serializers.ReadOnlyField()
+    hash = serializers.SerializerMethodField()
+
+    def get_hash(self, obj):
+        data = {
+            'closed': obj.closed,
+            'blocked': obj.blocked,
+            'sound': obj.sound,
+            'acl': obj.acl
+        }
+        return self.get_hash_from(data)
 
     class Meta:
         model = Lock
@@ -42,40 +62,46 @@ class LockSerializer(DeviceSerializer):
         read_only_fields = ("id", "uid", "timestamp", "alert", "acl", "online")
 
 
-class LockMQTTSerializer(DeviceSerializer):
-    """ Lock serializer for internal ops and MQTT """
-
-    topic = 'lock'
-
-    acl = serializers.ReadOnlyField()
-
-    class Meta:
-        model = Lock
-        exclude = ('id', 'online')
-        read_only_fields = ("id", "uid", "timestamp", "alert", "acl")
-
-
-class TerminalSerializer(DeviceSerializer):
+class TerminalInternalSerializer(DeviceSerializer):
     """ Terminal serializer """
 
     topic = 'terminal'
 
     online = serializers.ReadOnlyField()
-    mode_list = serializers.SerializerMethodField()
-    mode_switch = serializers.SerializerMethodField()
-    file_list = serializers.SerializerMethodField()
 
     class Meta:
         model = Terminal
         exclude = ('modes_normal', 'modes_extended')
         read_only_fields = ('id', 'uid', 'timestamp', 'alert')
 
+
+class TerminalSerializer(TerminalInternalSerializer):
+    """"""
+
+    mode_list = serializers.SerializerMethodField()
+    mode_switch = serializers.SerializerMethodField()
+    file_list = serializers.SerializerMethodField()
+    hash = serializers.SerializerMethodField()
+    online = None
+
+    class Meta:
+        model = Terminal
+        exclude = ('id', 'ip', 'uid', 'modes_normal', 'modes_extended', 'info', 'override')
+
+    def get_hash(self, obj):
+        data = {
+            'hacked': obj.hacked,
+            'blocked': obj.blocked,
+            'powered': obj.powered,
+            'mode_list': self.get_mode_list(obj),
+        }
+        return self.get_hash_from(data)
+
     def get_mode_switch(self, obj):
         result = {}
         for mode in obj.modes():
             serialized = WorkModeSerializer(mode, context=self.context)
             data = serialized.data
-            print(mode)
             for state in data.get("state", []):
                 if not result.get(state):
                     result[state] = {
@@ -101,16 +127,3 @@ class TerminalSerializer(DeviceSerializer):
         for mode in obj.modes():
             result.update(**mode.has_files)
         return result
-
-
-class TerminalMQTTSerializer(DeviceSerializer):
-    """ Terminal serializer """
-
-    topic = 'terminal'
-    file_list = serializers.ReadOnlyField()
-    mode_list = serializers.ReadOnlyField()
-    alert = serializers.ReadOnlyField()
-
-    class Meta:
-        model = Terminal
-        exclude = ("id", "info", "override", "ip", "modes_normal", "modes_extended")
