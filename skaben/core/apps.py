46ba0551459790dda75abc9cf0ff147fae6d62e8
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2021-11-20 10:09:29 +0300

    Откачен запуск воркеров отдельным контейнером

--- skaben/core/apps.py
+++ skaben/core/apps.py
@@ -9,6 +9,11 @@ class CoreConfig(AppConfig):
     name = 'core'
 
     def ready(self):
+        from transport.tasks.main import run_pinger, run_workers
         if 'runserver' in sys.argv:
             if os.environ.get('RUN_MAIN') == 'true':
+                print('starting workers')
+                run_workers()
+                print('starting pinger')
                 print('********** SKABEN SYSTEMS UP. SERVING THE OMNISSIAH **********')
+                run_pinger()
