4e3578982bfe276dddc0bbaaa38d6783b7293456
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-11-24 20:10:23 +0400

    Workers now starts in threads

--- skaben/core/management/commands/start_workers.py
+++ skaben/core/management/commands/start_workers.py
@@ -1,12 +1,37 @@
 from django.core.management.base import BaseCommand
+from core.transport.handlers import (
+    BaseHandler,
+    TranslatorHandler,
+    InternalHandler,
+    StateUpdateHandler,
+    ClientUpdateHandler,
+)
+from core.transport.config import get_mq_config, SkabenQueue
+from threading import Thread
+
+config = get_mq_config()
 
 
 class Command(BaseCommand):
     """ Django command to start workers for RabbitMQ"""
 
+    @staticmethod
+    def bind_handler(handler: type(BaseHandler)) -> type(BaseHandler):
+        # atm only to one
+        return handler(
+            config,
+            config.queues.get(handler.incoming_mark)
+        )
+
     def handle(self, *args, **options):
-        print('not implemented')
-        # run_workers()
-        # self.stdout.write(self.style.SUCCESS('workers started!'))
-        # run_pinger()
-        # self.stdout.write(self.style.SUCCESS('pingers started!'))
+        for handler in [
+            TranslatorHandler,
+            InternalHandler,
+            StateUpdateHandler,
+            ClientUpdateHandler,
+        ]:
+            worker = self.bind_handler(handler)
+            thread = Thread(target=worker.run, name=f'{handler.incoming_mark}_thread')
+            thread.start()
+            self.stdout.write(self.style.SUCCESS(f'[+] worker thread started: {worker}'))
+
