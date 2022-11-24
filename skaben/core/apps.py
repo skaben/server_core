cecd5d29088ccb11860aa0535ed84205076e1a81
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-11-24 20:11:11 +0400

    Transport MQ config loads on start

--- skaben/core/apps.py
+++ skaben/core/apps.py
@@ -1,19 +1,14 @@
 import os
 import sys
+import logging
 
 from django.apps import AppConfig
+from core.transport.config import get_mq_config
 
 
 class CoreConfig(AppConfig):
     name = 'core'
 
     def ready(self):
-        print('ready')
-        # from server_core.skaben.core.transport.main import run_pinger, run_workers
-        # if 'runserver' in sys.argv:
-        #     if os.environ.get('RUN_MAIN') == 'true':
-        #         print('starting workers')
-        #         run_workers()
-        #         print('starting pinger')
-        #         print('********** SKABEN SYSTEMS UP. SERVING THE OMNISSIAH **********')
-        #         run_pinger()
+        """do something on app start"""
+        get_mq_config()
