ed703067a3b724dacb978a1ce62c3e7d606d521e
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2024-05-10 19:03:29 +0200

    fix: device contexts typing

--- server/event_contexts/device/events.py
+++ server/event_contexts/device/events.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, List
+from typing import Optional, Dict, List, Union
 from pydantic import ValidationError, field_validator
 
 from core.transport.topics import SkabenTopics
@@ -14,7 +14,7 @@ class SkabenDeviceEvent(SkabenEvent):
     event_type: str = "device"
     device_type: str
     device_uid: Optional[str]
-    payload: Optional[Dict[str, any]] = {}
+    payload: Optional[Dict[str, Union[str, int, bool, dict, list]]] = {}
 
     @property
     def headers(self) -> List[str]:
